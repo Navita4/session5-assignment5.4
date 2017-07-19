e1ceba7fd73944ab393fd2134fe2881d84687cda
Navita4 <navita441992@gmail.com>
2017-07-19 10:16:57 +0530

    Add files via upload

---
+++ app4.js
@@ -0,0 +1,14 @@
+var employee =[{
+   name: "Aarsh",
+    age: 20,
+    salary: 40000,
+    city: "Banglore",
+    state: "Karnataka",
+    pincode: 556600
+}];
+console.log(employee[0].name);
+console.log(employee[0].age);
+console.log(employee[0].salary);
+console.log(employee[0].city);
+console.log(employee[0].state);
+console.log(employee[0].pincode);
